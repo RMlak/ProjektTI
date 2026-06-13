cabff38aa4f334b17c62327881ea8c149fee4002
Szmidek <mszmid@student.agh.edu.pl>
2026-06-13 19:11:29 +0200

    Uporządkowanie struktury projektu

---
+++ TimesheetAPI/Controllers/EmployeesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TimesheetAPI.Data;
+using TimesheetAPI.Models;
+
+namespace TimesheetAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EmployeesController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    // Wstrzykiwanie naszej bazy danych do kontrolera
+    public EmployeesController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: api/employees (Pobieranie wszystkich pracowników z bazy)
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+    {
+        return await _context.Employees.ToListAsync();
+    }
+
+    // POST: api/employees (Dodawanie nowego pracownika do bazy)
+    [HttpPost]
+    public async Task<ActionResult<Employee>> CreateEmployee(Employee employee)
+    {
+        _context.Employees.Add(employee);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetEmployees), new { id = employee.Id }, employee);
+    }
+}
